c6a38a5848138612bd875c2cfaf68f8bfcf85933
bartolomej <bartolomej.kozorog@gmail.com>
2019-12-05 21:51:50 +0100

    Updates

---
+++ precache-manifest.7ab1dc30ab2e54f15a116dbaf2a5eb47.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eccc388c6145a115150134bac335f1f",
+    "url": "/nike-website/index.html"
+  },
+  {
+    "revision": "59349372267bf5fd09ca",
+    "url": "/nike-website/static/css/main.e38550de.chunk.css"
+  },
+  {
+    "revision": "ca9ab33dac31f54f9dce",
+    "url": "/nike-website/static/js/2.5d584414.chunk.js"
+  },
+  {
+    "revision": "59349372267bf5fd09ca",
+    "url": "/nike-website/static/js/main.ea001f8a.chunk.js"
+  },
+  {
+    "revision": "dffa4957a2b39d79a71f",
+    "url": "/nike-website/static/js/runtime-main.d29a6b58.js"
+  },
+  {
+    "revision": "4c6696df5aee5638ffa3ae50fae41167",
+    "url": "/nike-website/static/media/awesome-facebook.4c6696df.svg"
+  },
+  {
+    "revision": "8549c9ed6634b4bb8552988c816f7107",
+    "url": "/nike-website/static/media/awesome-instagram.8549c9ed.svg"
+  },
+  {
+    "revision": "acc526a887942b90c681b6a7c0d1f6d0",
+    "url": "/nike-website/static/media/bcg-pattern.acc526a8.svg"
+  },
+  {
+    "revision": "3e44251bba712498623ea45e2d82fbaf",
+    "url": "/nike-website/static/media/footer-bcg.3e44251b.svg"
+  },
+  {
+    "revision": "f6abe151832a6d1bcf58420969eb1b4a",
+    "url": "/nike-website/static/media/landing-bcg.f6abe151.svg"
+  },
+  {
+    "revision": "e7f78ca4f95bee20abc57578d3de0a14",
+    "url": "/nike-website/static/media/landing-shoe.e7f78ca4.svg"
+  },
+  {
+    "revision": "9d531402b1ff7641e79e3cd5f9cdfb2f",
+    "url": "/nike-website/static/media/nike-badge.9d531402.svg"
+  },
+  {
+    "revision": "1365123aeb7b0cd395a8157508773dff",
+    "url": "/nike-website/static/media/runner.1365123a.svg"
+  },
+  {
+    "revision": "836a63fdf635bf498d8a269c08a3cfa9",
+    "url": "/nike-website/static/media/showcase-bcg.836a63fd.svg"
+  }
+]);
